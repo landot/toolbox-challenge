03cd5977b0face551e7ed706dccebd4570f7afd6
Tim <timowland@gmail.com>
2014-12-02 22:48:50 -0800

    general shape is done and timer is set up

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ var matchNumber = 0;
 var remainingPairs = 8;
 var missed = 0;
 var tilesBeingUsed = [];
+var timeCount;
 
 var i;
 for(i = 1; i <= 32; i++) {
@@ -22,6 +23,9 @@ $(document).ready(function() {
     $('#begin').click(function() {
         $('#win').css('display', 'none');
         tilesBeingUsed = [];
+        matchNumber += 1;
+        missed = 0;
+        remainingPairs = 8;
         console.log('game has started');
         //randomly picks 8 of the 32 tiles
         tileArray = _.sample(tileArray, 8);
@@ -36,13 +40,32 @@ $(document).ready(function() {
         //selects board from the html file
         var board = $('#board');
         var row = $(document.createElement('div'));
+        var img;
+        //creates the 4x4 grid of pictures
+        _.forEach(tilesBeingUsed, function(tile, elemIndex) {
+            if(elemIndex > 0 && 0 == elemIndex % 4) {
+               board.append(row);
+               row = $(document.createElement('div'));
+            }
+            img = $(document.createElement('img'));
 
-        
+            img.attr({
+                src: 'img/tile-back.png',
+                alt: 'tile ' + tile.idNumber
+           });
 
+            img.data('tile', tile);
+            row.append(img);
+        });
 
+        board.append(row);
 
-
-
+        var startTime = Date.now();
+        timeCount = window.setInterval(function() {
+            var timer = (Date.now() - startTime) / 1000;
+            timer = Math.floor(timer);
+            $('#timeElapsed').text(timer + ' seconds');
+        }, 1000);
 
     });
 });
